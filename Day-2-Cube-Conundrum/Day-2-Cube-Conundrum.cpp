77316ada3d4d1a6f96aabbc3f215efceeab745b9
HX73D <HX73D1337@gmail.com>
2023-12-20 22:30:29 +0100

    Day 2 part 1 Finished code is really ugly and contains a lot of unnecessary code

--- Day-2-Cube-Conundrum/Day-2-Cube-Conundrum.cpp
+++ Day-2-Cube-Conundrum/Day-2-Cube-Conundrum.cpp
@@ -5,18 +5,24 @@
 #include <algorithm>
 #include <regex>
 
-void checkPosibility(std::string line) {
+int gameId = 0;
+
+int checkPosibility(std::string line) {
+    std::string line1 = ";";
+    line1 += line += ";";
+    gameId += 1;
     std::string text;
     std::string oneRound;
     std::string green = "green";
     std::string red = "red";
     std::string blue = "blue";
-    int gameId = 1;
+    bool boolGreen = true;
+    bool boolBlue = true;
+    bool boolRed = true;
+    
     std::string currentRound;
-    
-    text += ";";
-    
-    for (char i : line) {
+
+    for (char i : line1) {
         
         if (text.find(green) != std::string::npos) {
             oneRound = text;
@@ -28,112 +34,102 @@ void checkPosibility(std::string line) {
                 char green1 = char(oneRound[IndexGreen - 3]);
                 char green2 = char(oneRound[IndexGreen - 2]);
 
-                std::cout << "Green: " << green1;
-                std::cout << green2 << std::endl << std::endl;
-                
                 if (int(green1) == 32) {
-                    std::cout << "Groen is kleiner dan 13" << std::endl;
+                    
                 }
-                else if (int(green1) == '1' && (int(green2) == '1' || int(green2) == '2' || int(green2) == '3')) {
-                    std::cout << "Groen is kleiner dan 1312" << std::endl;
+                else if (int(green1) == '1' && (int(green2) == 48 || int(green2) == '1' || int(green2) == '2' || int(green2) == '3')) {
+                    
                 }
                 else {
-                    std::cout << "Green is bigger than 13";
+                    boolGreen = false;
                 }
 
             }
             
-            
+            text.replace(text.find(green), 4, " ");
 
-            text.clear();
+            
         }
         if (text.find(blue) != std::string::npos) {
             oneRound = text;
-
-
 
             if (text.find(';') != std::string::npos) {
                 int IndexBlue = oneRound.find(blue);
                 char blue1 = char(oneRound[IndexBlue - 3]);
                 char blue2 = char(oneRound[IndexBlue - 2]);
 
-                std::cout << "Blue: " << blue1;
-                std::cout << blue2 << std::endl << std::endl;
-
                 if (int(blue1) == 32) {
-                    std::cout << "blue is kleiner dan 14" << std::endl;
+
                 }
-                else if (int(blue1) == '1' && (int(blue2) == '1' || int(blue2) == '2' || int(blue2) == '3' || int(blue2) == '4')) {
-                    std::cout << "blue is kleiner dan 14" << std::endl;
+                else if (int(blue1) == '1' && (int(blue2) == '0' || int(blue2) == '1' || int(blue2) == '2' || int(blue2) == '3' || int(blue2) == '4')) {
+
                 }
                 else {
-                    std::cout << "blue is bigger than 14";
+                    boolBlue = false;
                 }
 
             }
 
-
-
-            text.clear();
+            text.replace(text.find(blue), 4, " ");
+           
         }
         if (text.find(red) != std::string::npos) {
             oneRound = text;
-
-
 
             if (text.find(';') != std::string::npos) {
                 int IndexRed = oneRound.find(red);
                 char red1 = char(oneRound[IndexRed - 3]);
                 char red2 = char(oneRound[IndexRed - 2]);
 
-                std::cout << "red: " << red1;
-                std::cout << red2 << std::endl << std::endl;
-
                 if (int(red1) == 32) {
-                    std::cout << "red is kleiner dan 13" << std::endl;
+
                 }
-                else if (int(red1) == '1' && (int(red2) == '1' || int(red2) == '2')) {
-                    std::cout << "red is kleiner dan 13" << std::endl;
+                else if (int(red1) == '1' && (int(red2) == '0' || int(red2) == '1' || int(red2) == '2')) {
+
                 }
                 else {
-                    std::cout << "red is bigger than 13";
+                    boolRed = false;
                 }
-
             }
-
-
-
-            text.clear();
+            text.replace(text.find(red), 4, " ");
         }
         text += i;
     }
-    ++gameId;
+
+    if (boolGreen && boolRed && boolBlue) {
+        std::cout << gameId << std::endl;
+        return gameId;
+    }
+    else {
+        return 0;
+    }
 
 }
 
-void getFileData() {
+int getFileData() {
 
     std::ifstream file("Data.txt");
     std::string line;
     int redCubes = 12;
     int greenCubes = 13;
     int blueCubes = 14;
+    int total = 0;
 
     if (file.is_open()) {
         while (getline(file, line)) {
-            checkPosibility(line);
+            total += checkPosibility(line);
         }
         file.close();
     }
     else {
         std::cout << "Unable to open file";
     }
-
+    return total;
 }
 
 int main() {
 
-    getFileData();
+    std::cout << getFileData();
 
     return 0;
 }
